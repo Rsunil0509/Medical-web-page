80b5ee3ba57333364e9e0eb03bb20cd491ad21ef
Rsunil0509 <rsunil0509@gmail.com>
2025-01-13 15:48:01 +0530

    Add files via upload

--- medicine.js
+++ medicine.js
@@ -38,19 +38,30 @@ function togglePage(pageId) {
     const homePage = document.getElementById('home');
     const aboutPage = document.getElementById('about');
     const treatmentPage = document.getElementById('treatment');
+    const visitpage = document.getElementById('visit');
    
     if (pageId === 'home') {
         homePage.classList.remove('hidden');
         aboutPage.classList.add('hidden');
         treatmentPage.classList.add('hidden');
+        visitpage.classList.add('hidden');
     } else if (pageId === 'about') {
         homePage.classList.add('hidden');
         aboutPage.classList.remove('hidden');
         treatmentPage.classList.add('hidden');
+        visitpage.classList.add('hidden');
     }
     else if (pageId === 'treatment') {
         homePage.classList.add('hidden');
         aboutPage.classList.add('hidden');
         treatmentPage.classList.remove('hidden');
+        visitpage.classList.remove('hidden');
+    }
+    else if (pageId === 'visit') {
+        homePage.classList.add('hidden');
+        aboutPage.classList.add('hidden');
+        treatmentPage.classList.add('hidden');
+        visitpage.classList.remove('hidden');
+
     }
 }
